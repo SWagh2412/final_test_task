d3494b36cf851788e1d85b9818fff97138f6145e
SWagh2412 <sawagh@bestpeers.com>
2022-05-11 09:54:30 +0530

    Modified Validations

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -9,11 +9,10 @@ class Beer < ApplicationRecord
 
 	def check_beer_type
 		# byebug
-		unless ['ipa', 'brown_ale', 'pilsner', 'lager', 'lambic', 'hefeweizen'].include?(beer_type.downcase)
-			errors.add(:beer_type, "Can't be created/Already Present")
-		end
 
-		unless ['ipa', 'brown_ale', 'pilsner', 'lager', 'lambic', 'hefeweizen'].include?(beer_type.upcase)
+		beertype= ['ipa', 'brown_ale', 'pilsner', 'lager', 'lambic', 'hefeweizen']
+
+		unless ( beertype.include?(beer_type.downcase) && beertype.include?(beer_type.upcase) )
 			errors.add(:beer_type, "Can't be created/Already Present")
 		end
 	end
